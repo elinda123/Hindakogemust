f145b464ea2aab0b6ebd36b5923b6b0c81c474d6
Elinda Tragel <elinda123@users.noreply.github.com>
2017-06-16 20:18:01 +0300

    Change security configuration for places API

--- src/main/java/me/elinda/hindakogemust/config/SecurityConfiguration.java
+++ src/main/java/me/elinda/hindakogemust/config/SecurityConfiguration.java
@@ -99,6 +99,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
             .authorizeRequests()
+            .antMatchers("/api/places").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
