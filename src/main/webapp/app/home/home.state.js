3d57cbf6e1307955ec6ff2fee42bdc0ff8ca133e
Elinda Tragel <elinda123@users.noreply.github.com>
2017-06-16 19:46:31 +0300

    Show places on homepage

--- src/main/webapp/app/home/home.state.js
+++ src/main/webapp/app/home/home.state.js
@@ -16,14 +16,36 @@
             },
             views: {
                 'content@': {
-                    templateUrl: 'app/home/home.html',
-                    controller: 'HomeController',
+                    templateUrl: 'app/entities/place/places.html',
+                    controller: 'PlaceController',
                     controllerAs: 'vm'
                 }
             },
+            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                },
+                search: null
+            },
             resolve: {
-                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
-                    $translatePartialLoader.addPart('home');
+                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort),
+                        search: $stateParams.search
+                    };
+                }],
+                translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                    $translatePartialLoader.addPart('place');
+                    $translatePartialLoader.addPart('placeType');
+                    $translatePartialLoader.addPart('global');
                     return $translate.refresh();
                 }]
             }
