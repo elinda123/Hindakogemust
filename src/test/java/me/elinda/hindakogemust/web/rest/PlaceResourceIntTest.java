f86253c83f3d7e82e71bec695f8c0019943e9e60
Elinda Tragel <elinda123@users.noreply.github.com>
2017-06-16 19:19:11 +0300

    Add lost files

---
+++ src/test/java/me/elinda/hindakogemust/web/rest/PlaceResourceIntTest.java
@@ -0,0 +1,344 @@
+package me.elinda.hindakogemust.web.rest;
+
+import me.elinda.hindakogemust.HindakogemustApp;
+
+import me.elinda.hindakogemust.domain.Place;
+import me.elinda.hindakogemust.repository.PlaceRepository;
+import me.elinda.hindakogemust.repository.search.PlaceSearchRepository;
+import me.elinda.hindakogemust.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import me.elinda.hindakogemust.domain.enumeration.PlaceType;
+/**
+ * Test class for the PlaceResource REST controller.
+ *
+ * @see PlaceResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HindakogemustApp.class)
+public class PlaceResourceIntTest {
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    private static final String DEFAULT_ADDRESS = "AAAAAAAAAA";
+    private static final String UPDATED_ADDRESS = "BBBBBBBBBB";
+
+    private static final PlaceType DEFAULT_TYPE = PlaceType.CAFE;
+    private static final PlaceType UPDATED_TYPE = PlaceType.RESTAURANT;
+
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    @Autowired
+    private PlaceSearchRepository placeSearchRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    @Autowired
+    private EntityManager em;
+
+    private MockMvc restPlaceMockMvc;
+
+    private Place place;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        PlaceResource placeResource = new PlaceResource(placeRepository, placeSearchRepository);
+        this.restPlaceMockMvc = MockMvcBuilders.standaloneSetup(placeResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Place createEntity(EntityManager em) {
+        Place place = new Place()
+            .name(DEFAULT_NAME)
+            .address(DEFAULT_ADDRESS)
+            .type(DEFAULT_TYPE);
+        return place;
+    }
+
+    @Before
+    public void initTest() {
+        placeSearchRepository.deleteAll();
+        place = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void createPlace() throws Exception {
+        int databaseSizeBeforeCreate = placeRepository.findAll().size();
+
+        // Create the Place
+        restPlaceMockMvc.perform(post("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(place)))
+            .andExpect(status().isCreated());
+
+        // Validate the Place in the database
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeCreate + 1);
+        Place testPlace = placeList.get(placeList.size() - 1);
+        assertThat(testPlace.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(testPlace.getAddress()).isEqualTo(DEFAULT_ADDRESS);
+        assertThat(testPlace.getType()).isEqualTo(DEFAULT_TYPE);
+
+        // Validate the Place in Elasticsearch
+        Place placeEs = placeSearchRepository.findOne(testPlace.getId());
+        assertThat(placeEs).isEqualToComparingFieldByField(testPlace);
+    }
+
+    @Test
+    @Transactional
+    public void createPlaceWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = placeRepository.findAll().size();
+
+        // Create the Place with an existing ID
+        place.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restPlaceMockMvc.perform(post("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(place)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Alice in the database
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void checkNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = placeRepository.findAll().size();
+        // set the field null
+        place.setName(null);
+
+        // Create the Place, which fails.
+
+        restPlaceMockMvc.perform(post("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(place)))
+            .andExpect(status().isBadRequest());
+
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkAddressIsRequired() throws Exception {
+        int databaseSizeBeforeTest = placeRepository.findAll().size();
+        // set the field null
+        place.setAddress(null);
+
+        // Create the Place, which fails.
+
+        restPlaceMockMvc.perform(post("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(place)))
+            .andExpect(status().isBadRequest());
+
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void checkTypeIsRequired() throws Exception {
+        int databaseSizeBeforeTest = placeRepository.findAll().size();
+        // set the field null
+        place.setType(null);
+
+        // Create the Place, which fails.
+
+        restPlaceMockMvc.perform(post("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(place)))
+            .andExpect(status().isBadRequest());
+
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    @Transactional
+    public void getAllPlaces() throws Exception {
+        // Initialize the database
+        placeRepository.saveAndFlush(place);
+
+        // Get all the placeList
+        restPlaceMockMvc.perform(get("/api/places?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(place.getId().intValue())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())))
+            .andExpect(jsonPath("$.[*].address").value(hasItem(DEFAULT_ADDRESS.toString())))
+            .andExpect(jsonPath("$.[*].type").value(hasItem(DEFAULT_TYPE.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void getPlace() throws Exception {
+        // Initialize the database
+        placeRepository.saveAndFlush(place);
+
+        // Get the place
+        restPlaceMockMvc.perform(get("/api/places/{id}", place.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(place.getId().intValue()))
+            .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()))
+            .andExpect(jsonPath("$.address").value(DEFAULT_ADDRESS.toString()))
+            .andExpect(jsonPath("$.type").value(DEFAULT_TYPE.toString()));
+    }
+
+    @Test
+    @Transactional
+    public void getNonExistingPlace() throws Exception {
+        // Get the place
+        restPlaceMockMvc.perform(get("/api/places/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updatePlace() throws Exception {
+        // Initialize the database
+        placeRepository.saveAndFlush(place);
+        placeSearchRepository.save(place);
+        int databaseSizeBeforeUpdate = placeRepository.findAll().size();
+
+        // Update the place
+        Place updatedPlace = placeRepository.findOne(place.getId());
+        updatedPlace
+            .name(UPDATED_NAME)
+            .address(UPDATED_ADDRESS)
+            .type(UPDATED_TYPE);
+
+        restPlaceMockMvc.perform(put("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedPlace)))
+            .andExpect(status().isOk());
+
+        // Validate the Place in the database
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeUpdate);
+        Place testPlace = placeList.get(placeList.size() - 1);
+        assertThat(testPlace.getName()).isEqualTo(UPDATED_NAME);
+        assertThat(testPlace.getAddress()).isEqualTo(UPDATED_ADDRESS);
+        assertThat(testPlace.getType()).isEqualTo(UPDATED_TYPE);
+
+        // Validate the Place in Elasticsearch
+        Place placeEs = placeSearchRepository.findOne(testPlace.getId());
+        assertThat(placeEs).isEqualToComparingFieldByField(testPlace);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingPlace() throws Exception {
+        int databaseSizeBeforeUpdate = placeRepository.findAll().size();
+
+        // Create the Place
+
+        // If the entity doesn't have an ID, it will be created instead of just being updated
+        restPlaceMockMvc.perform(put("/api/places")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(place)))
+            .andExpect(status().isCreated());
+
+        // Validate the Place in the database
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeUpdate + 1);
+    }
+
+    @Test
+    @Transactional
+    public void deletePlace() throws Exception {
+        // Initialize the database
+        placeRepository.saveAndFlush(place);
+        placeSearchRepository.save(place);
+        int databaseSizeBeforeDelete = placeRepository.findAll().size();
+
+        // Get the place
+        restPlaceMockMvc.perform(delete("/api/places/{id}", place.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate Elasticsearch is empty
+        boolean placeExistsInEs = placeSearchRepository.exists(place.getId());
+        assertThat(placeExistsInEs).isFalse();
+
+        // Validate the database is empty
+        List<Place> placeList = placeRepository.findAll();
+        assertThat(placeList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    @Transactional
+    public void searchPlace() throws Exception {
+        // Initialize the database
+        placeRepository.saveAndFlush(place);
+        placeSearchRepository.save(place);
+
+        // Search the place
+        restPlaceMockMvc.perform(get("/api/_search/places?query=id:" + place.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(place.getId().intValue())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())))
+            .andExpect(jsonPath("$.[*].address").value(hasItem(DEFAULT_ADDRESS.toString())))
+            .andExpect(jsonPath("$.[*].type").value(hasItem(DEFAULT_TYPE.toString())));
+    }
+
+    @Test
+    @Transactional
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Place.class);
+        Place place1 = new Place();
+        place1.setId(1L);
+        Place place2 = new Place();
+        place2.setId(place1.getId());
+        assertThat(place1).isEqualTo(place2);
+        place2.setId(2L);
+        assertThat(place1).isNotEqualTo(place2);
+        place1.setId(null);
+        assertThat(place1).isNotEqualTo(place2);
+    }
+}
