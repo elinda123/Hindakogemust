41add7eb2a9f18b36c51b4e932b2d8ba0297799e
Elinda Tragel <elinda123@users.noreply.github.com>
2017-06-18 12:58:22 +0300

    Insert some places to database

---
+++ src/main/resources/config/liquibase/changelog/20170618000000_insert_restaurants.sql
@@ -0,0 +1,5 @@
+INSERT INTO place (name, address, jhi_type) VALUES ('Vapiano Solaris', 'Estonia pst. 9', 'RESTAURANT');
+INSERT INTO place (name, address, jhi_type) VALUES ('Mc Donald''s', 'Mahtra 29, 13811 Tallinn', 'RESTAURANT');
+INSERT INTO place (name, address, jhi_type) VALUES ('Katusekohvik', 'Viru 13/15, Tallinna vanalinn', 'CAFE');
+INSERT INTO place (name, address, jhi_type) VALUES ('St. Patrick''s, Pirita', 'Merivälja tee 5, 11911 Tallinn', 'CAFE');
+INSERT INTO place (name, address, jhi_type) VALUES ('Tommi Grill', 'Tatari 64, 10134 Tallinn', 'RESTAURANT');
